93c933e23a21dd06132413270dc8a96719c0bb1c
LIUSHULIN\shuli <shulinliu@aliyun.com>
2019-02-20 23:27:39 +0800

    Titanic 获救预测项目

--- Titanic/test_titanic.py
+++ Titanic/test_titanic.py
@@ -10,7 +10,8 @@ import numpy as np
 from matplotlib import pyplot as plt
 from sklearn.linear_model import LinearRegression, LogisticRegression
 from sklearn.model_selection import KFold,  cross_val_score
-from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import RandomForestClassifier, GradientBoostingClassifier
+from sklearn.feature_selection import SelectKBest, f_classif
 import re
 
 titanic = pd.read_csv("titanic_train.csv")
@@ -101,3 +102,65 @@ print(pd.value_counts(titles))
 # Map each title to an integer. Some titles are very rare, and are compressed into the same codes as other titles
 title_mapping = {"Mr": 1, "Miss": 2, "Mrs": 3, "Master": 4, "Dr": 5, "Rev": 6, "Major": 7, "Col": 7, "Mlle": 8,
                  "Mme": 8, "Don": 9, "Lady": 10, "Countess": 10, "Jonkheer": 10, "Sir": 9, "Capt": 7, "Ms": 2}
+for k, v in title_mapping.items():
+    titles[titles == k] = v
+# verify that we converted everything
+print(pd.value_counts(titles))
+# add in the title column
+titanic["Titles"] = titles
+
+# perform feature selection
+predictors = ["Pclass", "Sex", "Age", "SibSp", "Parch", "Fare", "Embarked", "FamilySize", "Titles", "NameLength"]
+selector = SelectKBest(f_classif, k=5)
+selector.fit(titanic[predictors], titanic["Survived"])
+
+# get the raw p-values for each feature, and transform from p-values into scores
+scores = -np.log10(selector.pvalues_)
+
+# plot the scores. See which is the best
+plt.bar(range(len(predictors)), scores)
+plt.xticks(range(len(predictors)), predictors, rotation="vertical")
+plt.show()
+
+# Use the best 5 features
+predictors = ["Pclass", "Sex", "Titles", "Fare", "NameLength"]
+alg = RandomForestClassifier(random_state=1, n_estimators=50, min_samples_split=8, min_samples_leaf=4)
+scores = cross_val_score(alg, titanic[predictors], titanic["Survived"], cv=kf)
+print(scores.mean())
+
+# 使用多个分类器进行集合
+# The algorithms we want to ensemble.
+# We're using the more linear predictors for the logistic regression,
+# and everything with the gradient boosting classifier.
+algorithms = [
+    [GradientBoostingClassifier(random_state=1, n_estimators=25, max_depth=3), ["Pclass", "Sex", "Age", "Fare", "Embarked", "FamilySize", "Titles"]],
+    [LogisticRegression(random_state=1), ["Pclass", "Sex", "Fare", "FamilySize", "Titles", "Age", "Embarked"]]
+]
+
+# Initialize the cross validation folds
+kf = KFold(n_splits=3, random_state=1)
+
+predictions = []
+for train_index, test_index in kf.split(titanic[predictors], titanic["Survived"]):
+    train_target = titanic["Survived"].iloc[train_index]
+    full_test_predictions = []
+    # Make predictions for each algorithm on each fields
+    for alg, predictors in algorithms:
+        # Fit the algorithm on the training data
+        alg.fit(titanic[predictors].loc[train_index, :], train_target)
+        # Select and predict on the test fold
+        test_predictions = alg.predict_proba(titanic[predictors].iloc[test_index, :].astype(float))[:, 1]
+        full_test_predictions.append(test_predictions)
+    # Use a simple ensembling scheme -- just average the predictions to get the final classification.
+    test_predictions = (full_test_predictions[0] + full_test_predictions[1]) / 2
+    # Any value over .5 is assumed to be a 1 prediction, and below .5 is a 0 prediction.
+    test_predictions[test_predictions <= .5] = 0
+    test_predictions[test_predictions > .5] = 1
+    predictions.append(test_predictions)
+
+# Put all the predictions together into one array.
+predictions = np.concatenate(predictions, axis=0)
+
+# Compute accuracy by comparing to the training data.
+accuracy = sum(predictions[predictions == titanic["Survived"]]) / len(predictions)
+print(accuracy)
